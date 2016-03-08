73805d6f6f2aa9bc87c8b48f8e854c9319188ee8
stefano fedele <fedelemail@yahoo.it>
2016-03-08 00:00:46 +0000

    Added files via upload

--- uvVisData.py
+++ uvVisData.py
@@ -27,22 +27,45 @@ def integTime(file_k): #find integral time
 path = r"C:\Users\fedel_000\Documents\Measurements\stefano\Reflectance_12_19_angle_resolved"
 s = '\\' # '\\' for windows and '/' for linux
 folders = os.listdir(path)
-
-j = 0            
-pathFiles = path + s + folders[j]
-listFiles = os.listdir(pathFiles)
-
-k = 0
-pol = ['noPol', '90Pol', '00Pol'] # different types of polarizations
 List = {} # dictionary of files by polarization
 ListSG = {}
 ListBG = {} # dictionary of backgrounds by polarization
+#Angle = {}
+reflectance = {}
+j = 0 
 
-for pol_k in pol:
-    List[ pol_k ] = sel( pol_k , listFiles) 
-    ListSG[ pol_k ] = sel( 'signal' , List[ pol_k ]) # dictionary of signals by polarization
-    ListBG[ pol_k ] = sel('background', List[ pol_k ])[0] # dictionary of backgrounds by polarizations    
-    for fileName in ListSG[ pol_k ]:    
-        Path = pathFiles + s + fileName
-        print pd.read_csv(Path, sep=',', header= None)[1].astype(float)/integTime(fileName)
+fig = plt.figure(figsize=(20,10))
+ax = fig.add_subplot(111)
+ax.axis([400, 900, 0, 1])
+Time = 1
+
+for angles in folders:    
+    print j       
+    pathFiles = path + s + folders[j]
+    listFiles = os.listdir(pathFiles)
+    wavelength = pd.read_csv(pathFiles + s + listFiles[0], sep=',', header= None)[0].astype(float)    
     
+    k = 0
+    pol = ['noPol', '90Pol', '00Pol'] # different types of polarizations
+    
+    #for pol_k in pol:
+    for pol_k in [ pol[2] ]: # it selects just one type of polarization
+        List[ pol_k ] = sel( pol_k , listFiles) 
+        ListBG[ pol_k ] = sel('background', List[ pol_k ])[0] # dictionary of backgrounds by polarizations            
+        ListSG[ pol_k ] = sel( 'signal' , List[ pol_k ]) # dictionary of signals by polarization
+        ListSG[ pol_k ] = sel( 'AuNR_angle' , ListSG[ pol_k ] ) # select spectra with ONLY Gold Nanorods
+        
+        #bg = pd.read_csv(pathFiles + s + ListBG[ pol[0] ], sep=',', header= None)[1].astype(float)/integTime( ListBG[ pol_k ] )        
+        bg = pd.read_csv(pathFiles + s + ListBG[ pol_k ], sep=',', header= None)[1].astype(float)/integTime( ListBG[ pol_k ] )
+        #set pol[0] on the previous line if you want all values normalized at the some background that is "noPol"
+        for fileName in ListSG[ pol_k ]:    
+            Path = pathFiles + s + fileName
+            signal = pd.read_csv(Path, sep=',', header= None)[1].astype(float)/integTime(fileName)
+            reflectance[ fileName ] = signal / bg
+            line, = ax.plot(wavelength, reflectance[ fileName ], label = fileName[:-4])
+            ax.legend(handles=[line])
+            plt.pause(Time)
+    
+    j = j + 1
+
+
